f8d78311745b1c87fcbf4af869b8a0ab9fe90dbf
semen-nenashev <nenashev.sd@gmail.com>
2021-01-29 15:45:48 +0200

    First Test done

--- src/ExampleException.java
+++ src/ExampleException.java
@@ -1,8 +1,12 @@
 public class ExampleException {
-  public static int rectangleArea(int a, int b) {
-    // TODO put your code here
-    return 0;
-  }
+    public static int rectangleArea(int a, int b) throws IllegalArgumentException {
+        if (a > 0 && b > 0) {
+            return a * b;
+        } else {
+            throw new IllegalArgumentException("input value is below zero!");
+        }
+    }
 }
+
 
 
--- src/ExampleExceptionTest.java
+++ src/ExampleExceptionTest.java
@@ -1,47 +1,51 @@
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.fail;
 
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ExampleExceptionTest {
 
-  /**
-   * Test data for positive test.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "data")
-  public static Object[][] data() {
-    return new Object[][]{
-        {2, 2, 4},
-        {2, 3, 6}
-        // TODO add 2 more test data here
-    };
-  }
+    /**
+     * Test data for positive test.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "data")
+    public static Object[][] data() {
+        return new Object[][]{
+                {2, 2, 4},
+                {2, 3, 6},
+                {4, 4, 16},
+                {5, 2, 10}
+        };
+    }
 
-  /**
-   * Test data for exception test.
-   *
-   * @return test data array
-   */
-  @DataProvider(name = "negativeData")
-  public static Object[][] negativeData() {
-    return new Object[][]{
-        {-2, 2},
-        {2, -2}
-        // TODO add 2 more test data here
-    };
-  }
+    /**
+     * Test data for exception test.
+     *
+     * @return test data array
+     */
+    @DataProvider(name = "negativeData")
+    public static Object[][] negativeData() {
+        return new Object[][]{
+                {-2, 2},
+                {2, -2},
+                {-3, 5},
+                {5, -7}
+        };
+    }
 
-  @Test(dataProvider = "data")
-  public void testRectangleArea(int a, int b, int c) {
-    // TODO put your code here
-  }
+    @Test(dataProvider = "data")
+    public void testRectangleArea(int a, int b, int c) {
+        Assert.assertEquals(ExampleException.rectangleArea(a, b), c, "input value is below zero!");
+    }
 
 
-  @Test(dataProvider = "negativeData")
-  public void testRectangleAreaNegative(int a, int b) {
-    // TODO put your code here
-  }
+    @Test(dataProvider = "negativeData")
+    public void testRectangleAreaNegative(int a, int b) {
+
+
+    }
 }
